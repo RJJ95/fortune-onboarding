3ecac3d195fb233b118aaed6ec6afd2dd7227af0
Ricardo de Vries <ricardo@MBP-van-Ricardo.lan>
2021-11-22 18:11:21 +0000

    did styling primary menu

--- src/components/sections/primary-menu/primary-menu-styles.js
+++ src/components/sections/primary-menu/primary-menu-styles.js
@@ -9,17 +9,24 @@ export const Wrapper = styled.nav`
   box-shadow: ${({ theme }) => theme.shadow};
   border-radius: 0 0 20px 20px;
   background-color: ${({ theme }) => theme.colors.white};
+  position: relative;
+  min-height: 76px;
 `;
 
 export const LogoContainer = styled.div`
   padding: 20px 40px;
   display: flex;
   align-items: center;
+  position: absolute;
 `;
 
 export const MenuItemsContainer = styled.div`
   display: flex;
   justify-content: space-between;
+
+  a:not(:last-child) {
+    padding-right: 50px;
+  }
 `;
 
 export const MenuItem = styled(Link)``;
@@ -27,4 +34,6 @@ export const MenuItem = styled(Link)``;
 export const VerticalLine = styled.div`
   border-left: 1px solid ${({ theme }) => theme.colors.silverMedal};
   height: 60px;
+  position: absolute;
+  left: 116px;
 `;
